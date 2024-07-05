f77f2198bea15ff848e8d43c69c58044124ff5bb
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-07-05 13:58:28 +0200

    sadas

--- public/scripts/checkout/checkoutHeder.js
+++ public/scripts/checkout/checkoutHeder.js
@@ -3,21 +3,21 @@ export function renderCheckoutHeader(){
   let generatedHTML = '';
   generatedHTML= `
     <div class="header-content">
-    <div class="checkout-header-left-section">
+    <div class="naplata-zaglavlje-left-section">
       <a href="/">
         <img class="beli-logo" src="slike/logo-black.png">
         <img class="beli-logo-telefon" src="slike/logo-black-mobile.png">
       </a>
     </div>
 
-    <div class="checkout-header-middle-section">
+    <div class="naplata-zaglavlje-middle-section">
       Stranica za naplatu (<a class="return-to-home-link js-return-to-home-link"
         href="/"></a>)
     </div>
 
-    <div class="checkout-header-right-section">
+    <div class="naplata-zaglavlje-right-section">
       <img src="slike/ikonice/naplata-kljuc-ikonica.png">
     </div>
   </div>`
-  document.querySelector('.js-checkout-header').innerHTML = generatedHTML;
+  document.querySelector('.js-naplata-zaglavlje').innerHTML = generatedHTML;
 }
