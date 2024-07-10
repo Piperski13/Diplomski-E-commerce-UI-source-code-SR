cc3854705fa179e2c1ca0e2bcf0d766af1170f6d
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-07-10 11:53:37 +0200

    Brisanje logike za mongo

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require('./aplikacija');
-
-const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
-
-mongoose
-  .connect(DB)
-  .then(() => console.log("Uspešno povezan sa bazom podataka"))
-  .catch((err) => console.error("Greška prilikom povezivanja sa bazom podataka:", err));
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
