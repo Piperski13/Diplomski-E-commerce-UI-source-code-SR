06c375853a8e760674bc77152ad05b7c5fdb3f0b
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-07-10 11:49:24 +0200

    Dodata baza podataka startni fajlovi

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const app = require('./aplikacija');
 
-const port = 3000;
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("Uspešno povezan sa bazom podataka"))
+  .catch((err) => console.error("Greška prilikom povezivanja sa bazom podataka:", err));
+
+const port = process.env.PORT || 3000;
 app.listen(port,()=>{
   console.log(`Port ${port} is waiting for a request...`);
 })
