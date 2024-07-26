52a2fc109c7f267b22f315f992a06d2d114eddaf
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-07-26 21:33:06 +0200

    Clean up

--- opstifolder/market.js
+++ opstifolder/market.js
@@ -1,4 +1,4 @@
-import {korpa, dodajUKorpu,izracunajKolicinuKorpe} from "../podaci/korpa.js"; 
+import {dodajUKorpu,izracunajKolicinuKorpe} from "../podaci/korpa.js"; 
 import {proizvodi,ucitavanjeProizvoda} from "../podaci/proizvodi.js";
 
 
@@ -33,7 +33,6 @@ function renederujProizvodeGrid(){
     });
   };
 
-  //gets proizvodi from proizvodi.js and generates html 
   filtriraniProizvodi.forEach((proizvod)=>{
     proizvodiHTML += `
       <div class="proizvod-kontejner">
@@ -107,33 +106,42 @@ function renederujProizvodeGrid(){
   })
 
   function azurirajKorpaKolicinu(){        
-    let korpaKolicina = izracunajKolicinuKorpe();   //korpa.js function that calculates korpa kolicina
-    if(!korpaKolicina){          //essentialy break a function if korpaKolicina undefined
+    let korpaKolicina = izracunajKolicinuKorpe();   
+
+    if(!korpaKolicina){          
       return;
     }
     document.querySelector('.js-kolicina-u-kolicima').innerHTML = korpaKolicina;
   }
 
-  function dodajUKorpuZeleno(proizvodId,timeoutObjekat){     // pop up msg function
-    let dodajPorukuElement = document.querySelector(`.js-dodaj-korpa-${proizvodId}`);    //target add korpa div with opacity 0
-        dodajPorukuElement.classList.add('dodato-u-korpu-kliknuto');                // and then give it a class with opacity 1
+  function dodajUKorpuZeleno(proizvodId,timeoutObjekat){     
+    let dodajPorukuElement = document.querySelector(`.js-dodaj-korpa-${proizvodId}`); 
+
+        dodajPorukuElement.classList.add('dodato-u-korpu-kliknuto');                
         
-        if (timeoutObjekat.timeoutId){               //if true, it means that interval exists, clear it else -> skip it
+        if (timeoutObjekat.timeoutId){               
+
           clearTimeout(timeoutObjekat.timeoutId);
         }
-        timeoutObjekat.timeoutId = setTimeout(()=>{        //removes class and return opacity to 0 in 2000ms 
-          dodajPorukuElement.classList.remove('dodato-u-korpu-kliknuto');      //it also stores interval into timeoutObjekat
-        },2000);                                      //so if we press it again we can clear interval with if statemant
+        timeoutObjekat.timeoutId = setTimeout(()=>{        
+
+          dodajPorukuElement.classList.remove('dodato-u-korpu-kliknuto');      
+
+        },2000);                                      
   }
 
-  //adds event listeners to add buttons
+  
   document.querySelectorAll('.js-dodaj-dugme')
     .forEach((button)=>{      
-      let dodatePorukeTimeouts = {};                           //create a object for checking interval dodajUKorpuZeleno
+      let dodatePorukeTimeouts = {};                           
+
       button.addEventListener('click',()=>{
-        const proizvodId = button.dataset.proizvodId;   //on click save data-proizvod-id in a const
+        const proizvodId = button.dataset.proizvodId;   
+
         dodajUKorpu(proizvodId);
-        dodajUKorpuZeleno(proizvodId,dodatePorukeTimeouts);  // for a green pop up msg function
+        
+        dodajUKorpuZeleno(proizvodId,dodatePorukeTimeouts);  
+
         azurirajKorpaKolicinu();
       });
   });
