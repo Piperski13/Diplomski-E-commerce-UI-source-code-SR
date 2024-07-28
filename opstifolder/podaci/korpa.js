0ac5421161d1d09f9f25661166c96c7007f1f9fb
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-07-28 16:00:18 +0200

    Clean up korpa.js

--- opstifolder/podaci/korpa.js
+++ opstifolder/podaci/korpa.js
@@ -10,27 +10,29 @@ function sacuvajUSkladiste(){
 }
 
 export function dodajUKorpu(proizvodId){
-  let odgovarajućiArtikal;           //boolean value
+  let odgovarajućiArtikal;           
   let kolicina;
   let izborKolicine;
       
-      korpa.forEach((korpaArtikal)=>{              //when button clicked goe trough korpa.js[], and if 
-        if(proizvodId === korpaArtikal.proizvodId ){    //it exists set item to matching item
+      korpa.forEach((korpaArtikal)=>{              
+
+        if(proizvodId === korpaArtikal.proizvodId ){    
+
           odgovarajućiArtikal = korpaArtikal;
         }
       });
-                    //gets value of drop down kolicina and stores it
+                    
       let kolicinaString = document.querySelector(`.js-izbor-kolicine-${proizvodId}`);
       if(kolicinaString){
         izborKolicine = kolicinaString.value
-        kolicina = Number(izborKolicine);         //DOM retruns string always, so make it num
+        kolicina = Number(izborKolicine);         
       }else{
         kolicina=1;
       }
       
     
-      if(odgovarajućiArtikal){              //if odgovarajućiArtikal exsits its making this statemant true thus
-        odgovarajućiArtikal.kolicina+=kolicina;     // it only increases its kolicina and skips korpa.push
+      if(odgovarajućiArtikal){              
+        odgovarajućiArtikal.kolicina+=kolicina;     
       }else{
         korpa.push({
           proizvodId,
@@ -41,12 +43,12 @@ export function dodajUKorpu(proizvodId){
   sacuvajUSkladiste();
 }
 export function izbrisiIzKorpe(proizvodId){
-  let novaKorpa = korpa.filter((korpaArtikal) => korpaArtikal.proizvodId !== proizvodId) //returns 
+  let novaKorpa = korpa.filter((korpaArtikal) => korpaArtikal.proizvodId !== proizvodId) 
   korpa = novaKorpa;
   sacuvajUSkladiste();
 }
 
-export function izracunajKolicinuKorpe(){    //calculates korpa kolicina and retuns its final value
+export function izracunajKolicinuKorpe(){    
   let korpaKolicina = 0;
 
   korpa.forEach(korpaArtikal => {
